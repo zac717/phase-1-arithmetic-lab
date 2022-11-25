e06093a802159d4cfb282bb4ea3ee36c9ee5d44d
isaac <isaacongera717@gmail.com>
2022-11-25 14:46:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = 2;
+Math.random();
+console.log(random);
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+console.log(mod);
+
+let num = (5,8,12,15,17,20)
+let max = Math.max(5,8,12,15,17,20)
+console.log(max);
+
+
+
